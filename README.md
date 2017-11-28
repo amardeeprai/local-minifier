319e4dfaaf09d452d97bfac128c54cb4d660a3d6
Amardeep Rai <amardeeprai@gmail.com>
2017-11-28 21:46:41 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# local-minifier
+
+This is a local environment that you can use to minify your CSS, JS, & HTML files.
+
+## Getting Started
+
+### Download / Clone
+
+Start by cloning or downloading the source files.
+
+Clone the repo using Git:
+
+```bash
+git clone https://github.com/amardeeprai/local-minifier.git
+```
+
+Alternatively you can [download](https://github.com/amardeeprai/local-minifier/archive/master.zip)
+this repository.
+
+
+### Install required modules
+
+Open your terminal window and change into the source diectory where you have cloned or downloaded the repo to. Run the command:
+
+```
+npm install
+```
+
+NOTE:You will need [node.js](https://nodejs.org/) installed on your computer before you can run this command.
+
+
+### Initialise the environment
+
+Open your terminal window and change into the source diectory where you have cloned or downloaded the repo to.
+
+Once all of the required node modules have been downloaded and installed, run the command
+
+```
+gulp
+```
+
+This will generate the CSS, JS, & HTML files.
+
+
+### Generating HTML files
+
+You can generate HTML files by running the following command:
+
+```
+gulp pages
+```
+
+NOTE: this can only be run from the directory where you have cloned or downloaded the repo to.
+
+
+### Generating CSS files
+
+You can generate CSS files by running the following command:
+
+```
+gulp styles
+```
+
+NOTE: this can only be run from the directory where you have cloned or downloaded the repo to.
+
+
+### Generating JS files
+
+You can generate JS files by running the following command:
+
+```
+gulp scripts
+```
+
+NOTE: this can only be run from the directory where you have cloned or downloaded the repo to.
+
+
+## License
+
+Copyright 2017 Amardeep Rai. Licensed under an
+[Apache-2](https://github.com/amardeeprai/local-minifier/blob/master/LICENSE)
+license.
---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+/**
+ *  Copyright 2017 Amardeep Rai
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+'use strict';
+
+var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+var del = require('del');
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+
+// Set the browser that you want to supoprt
+const AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+// Gulp task to minify CSS files
+gulp.task('styles', function () {
+  return gulp.src('./src/sass/styles.scss')
+    // Compile SASS files
+    .pipe(sass({
+      outputStyle: 'nested',
+      precision: 10,
+      includePaths: ['.'],
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    // Auto-prefix css styles for cross browser compatibility
+    .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    // Minify the file
+    .pipe(csso())
+    // Output
+    .pipe(gulp.dest('./dist/css'))
+});
+
+// Gulp task to minify JavaScript files
+gulp.task('scripts', function() {
+  return gulp.src('./src/js/**/*.js')
+    // Minify the file
+    .pipe(uglify())
+    // Output
+    .pipe(gulp.dest('./dist/js'))
+});
+
+// Gulp task to minify HTML files
+gulp.task('pages', function() {
+  return gulp.src(['./src/**/*.html'])
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+// Clean output directory
+gulp.task('clean', () => del(['dist']));
+
+// Gulp task to minify all files
+gulp.task('default', ['clean'], function () {
+  runSequence(
+    'styles',
+    'scripts',
+    'pages'
+  );
+});
